4f3b5288ee2e6bbd783b9742c6b5eeeb02593a1d
Seungpyo Suh <me@sspzoa.io>
2025-12-12 23:57:00 +0900

    feat: Add custom theme to GitHubCalendar component

--- src/app/(pages)/portfolio/(components)/contributions.tsx
+++ src/app/(pages)/portfolio/(components)/contributions.tsx
@@ -12,7 +12,25 @@ export const ContributionsSection = () => {
         target="_blank"
         rel="noreferrer noopener"
         className="flex w-full items-center justify-center rounded-radius-400 border border-line-outline p-spacing-400 duration-100 hover:border-core-accent hover:bg-core-accent-translucent">
-        <GitHubCalendar username="sspzoa" />
+        <GitHubCalendar
+          username="sspzoa"
+          theme={{
+            light: [
+              "var(--components-fill-standard-secondary)",
+              "rgba(109, 135, 168, 0.4)",
+              "rgba(109, 135, 168, 0.6)",
+              "rgba(109, 135, 168, 0.8)",
+              "var(--core-accent)",
+            ],
+            dark: [
+              "var(--components-fill-standard-secondary)",
+              "rgba(109, 135, 168, 0.4)",
+              "rgba(109, 135, 168, 0.6)",
+              "rgba(109, 135, 168, 0.8)",
+              "var(--core-accent)",
+            ],
+          }}
+        />
       </Link>
     </Section>
   );
